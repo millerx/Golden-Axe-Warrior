929e7bcd1e11693c105052242720fd7df656cde6
Chris Miller <millerx@gmail.com>
2026-01-03 15:59:33 -0500

    Move notes to notes.js

---
+++ notes.js
@@ -0,0 +1,40 @@
+window.COORD_NOTES = {
+  "6x8": "Town: Milver\n\
+  Inn: free\n\
+  Shop:\n\
+    Key: 60h\n\
+    Magic Feather: 60h\n\
+    Magic Oil: 130h",
+  "6x9": "Start here!",
+  "7x9": "Shop:\n\
+    Key: 40h\n\
+    Magic Feather: 40h\n\
+    Magic Oil: 70h",
+  "7x11": "Gamble",
+  "7x12": "TODO: Could not kill sea monster",
+  "8x8": "Horns",
+  "8x9": "Horns",
+  "8x12": "Save",
+  "8x13": "Temple 1: Axe, Hammer man",
+  "9x9": "Hint: Walk along the river in the forest.",
+  "9x11": "Town: Saylun\n\
+  Grave town\n\
+  I saw a Dwarf. It was in the south - near Lake Whale\n\
+  Inn: free\n\
+  Shop:\n\
+    Key: 60h\n\
+    Magic Feather: 60h\n\
+    Magic Oil: 130h",
+  "9x13": "Heart",
+  "10x9": "Save",
+  "10x10": "Temple 2: Torch, Dragon",
+  "10x11": "Shop:\n\
+    Key: 40h\n\
+    Magic Feather: 40h\n\
+    Magic Oil: 70h",
+  "11x11": "~Horns~",
+  "12x13": "Shop:\n\
+    Knight's Shield: 250h\n\
+    Meat: 30h\n\
+    Magic Oil: 110h"
+};
